eeee0fef4a0dc7226612d4af5bd7eb737aae16f4
aurorethomas <aurorethomas4@gmail.com>
2015-03-21 17:21:33 +0100

    entite Disponibilite*

---
+++ models/entities/Disponibilite.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class Disponibilite {
+	protected $IDDisponibilite;
+	protected $dateDisponibilite;
+	protected $heuredebutDisponibilite;
+	protected $heurefinDisponibilite;
+
+	function __construct($IDDisponibilite, $dateDisponibilite, $heuredebutDisponibilite, $heurefinDisponibilite) {
+        $this->IDDisponibilite = $IDDisponibilite;
+        $this->dateDisponibilite = $dateDisponibilite;
+        $this->heuredebutDisponibilite = $immatriculationVoiture;
+        $this->heurefinDisponibilite = $heurefinDisponibilite;
+    }
+}
+

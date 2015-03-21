6761e937aa263a56ef360d0beec3713f70ba0f8f
aurorethomas <aurorethomas4@gmail.com>
2015-03-21 22:38:39 +0100

    class Lieu

---
+++ models/entities/Lieu.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class Lieu {
+	protected $IDLieu;
+	protected $adresseLieu;
+	protected $villeLieu;
+	protected $codePostalLieu;
+	protected $longitudeLieu;
+	protected $latitudeLieu;
+
+	function __construct($IDLieu, $adresseLieu, $villeLieu, $codePostalLieu, $longitudeLieu,$latitudeLieu) {
+        $this->IDLieu = $IDLieu;
+        $this->adresseLieu = $adresseLieu;
+        $this->villeLieu = $villeLieu;
+        $this->codePostalLieu = $codePostalLieu;
+        $this->longitudeLieu = $longitudeLieu;
+        $this->latitudeLieu = $latitudeLieu;
+    }
+}
+

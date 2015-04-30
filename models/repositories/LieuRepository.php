b733a5f6702c2b052ff39b9dcc93d63e5621b155
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2015-04-30 15:19:19 +0200

    Saving new place working fine

--- models/repositories/LieuRepository.php
+++ models/repositories/LieuRepository.php
@@ -4,7 +4,19 @@ class LieuRepository{
 	
 	protected $db;
 
-	final public function __construct(Connection $db) {
+	final public function __construct($db) {
        	$this->db = $db;
     }
+
+	public function saveLieu(Lieu $lieu){
+		$sql = "INSERT INTO Lieu(adresseLieu, villeLieu, codePostalLieu) ";
+		$sql .= " VALUES(:adresse,:ville,:CP) ";
+		$statement = $this->db->prepare($sql);
+		$statement->bindValue("adresse", $lieu->getAdresseLieu());
+		$statement->bindValue("ville", $lieu->getVilleLieu());
+		$statement->bindValue("CP", $lieu->getCodePostalLieu());
+		$statement->execute();
+		// return the ID of the inserted Lieu
+		return $this->db->lastInsertId();
+	}
 }

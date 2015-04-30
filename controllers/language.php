1a4ddf99a198c99989d13a78064a4253e945ad7a
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2015-04-30 17:50:10 +0200

    Bugfix language

--- controllers/language.php
+++ controllers/language.php
@@ -1,6 +1,13 @@
 <?php
 
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
 $app->before(function (Request $request, Application $app) {
-    $language = $request->getPreferredLanguage(array('fr', 'en'));
+    if($request->get('_route') == "country_redirect"){
+        $language = $request->getPreferredLanguage(array('fr', 'en'));
+    }else{
+       $language = $request->getLocale();
+    }
     $app['session']->set('language', $language);
 });
